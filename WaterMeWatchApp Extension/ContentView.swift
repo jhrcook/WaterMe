b9f483e8cd11fcbf2234de88b20b5d7c9995c1f8
Josh Cook <joshuacook0023@gmail.com>
2020-07-31 07:53:34 -0400

    chore; create Apple Watch extension framework

---
+++ WaterMeWatchApp Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterMeWatchApp Extension
+//
+//  Created by Joshua on 7/31/20.
+//  Copyright © 2020 Joshua Cook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
