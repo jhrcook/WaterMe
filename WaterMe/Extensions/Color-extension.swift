b500d56e373822f923017dbcb26785077220173b
Josh Cook <joshuacook0023@gmail.com>
2020-08-01 20:56:22 -0400

    feat: manually added background colors -_-

--- WaterMe/Extensions/Color-extension.swift
+++ WaterMe/Extensions/Color-extension.swift
@@ -16,12 +16,26 @@ extension Color {
     ///   - adobeS: Saturation (out of 100)
     ///   - adobeB: Brightness (out of 100)
     init(adobeH: Double, adobeS: Double, adobeB: Double) {
-        self.init(hue: adobeH / 360.0, saturation: adobeS / 100.0, brightness: adobeB / 100.0)
+        self.init(hue: adobeH / 360.0,
+                  saturation: adobeS / 100.0,
+                  brightness: adobeB / 100.0)
     }
     
     static let darkBlue = Color(adobeH: 205, adobeS: 62, adobeB: 66)
     static let lightBlue = Color(adobeH: 205, adobeS: 52, adobeB: 96)
     static let lightTomato = Color(adobeH: 1, adobeS: 52, adobeB: 96)
+    
+    
+    init(red255: Double, green255: Double, blue255: Double) {
+        self.init(red: red255 / 255.0,
+                  green: green255 / 255.0,
+                  blue: blue255 / 255.0)
+    }
+    
+    static let mySystemGrey3 = Color(red255: 72, green255: 72, blue255: 74)
+    static let mySystemGrey4 = Color(red255: 58, green255: 58, blue255: 60)
+    static let mySystemGrey5 = Color(red255: 44, green255: 44, blue255: 46)
+    static let mySystemGrey6 = Color(red255: 28, green255: 28, blue255: 30)
 }
 
 
@@ -54,6 +68,11 @@ struct CustomColors_Previews: PreviewProvider {
             ExampleColorText(text: "Dark Blue", backgroundColor: .darkBlue)
             ExampleColorText(text: "Light Blue", backgroundColor: .lightBlue)
             ExampleColorText(text: "Light Tomato", backgroundColor: .lightTomato)
+            
+            ExampleColorText(text: "mySystemGrey3", backgroundColor: .mySystemGrey3)
+            ExampleColorText(text: "mySystemGrey4", backgroundColor: .mySystemGrey4)
+            ExampleColorText(text: "mySystemGrey5", backgroundColor: .mySystemGrey5)
+            ExampleColorText(text: "mySystemGrey6", backgroundColor: .mySystemGrey6)
         }
         
     }
